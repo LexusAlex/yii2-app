585b84e42037c9a163004f2ab15de7e3d582bb85
Alexsey <alexsey_89@bk.ru>
2016-12-19 18:02:40 +0300

    #13 Created core structure directories and tasks in gulpfile.js

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,9 +13,10 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     //public $sourcePath = '@frontend/web';
     public $css = [
-        'css/site.css',
+        'css/style.css',
     ];
     public $js = [
+        'js/main.js'
     ];
     public $depends = [
         //'yii\web\YiiAsset',
