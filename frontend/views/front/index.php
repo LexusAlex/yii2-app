4159c9fd2f6fd0ec0be293c4ffb60eee6896b8dd
Alexsey <alexsey_89@bk.ru>
2016-12-20 11:08:07 +0300

    #14 Template main page frontend

--- frontend/views/front/index.php
+++ frontend/views/front/index.php
@@ -1,4 +1,86 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-<?= __FILE__; ?>
+<section class="row__left">
+    <article class="article" role="article">
+        <header>
+            <h2><a href="#">Большое название статьи много текста разного разного разного разного</a></h2>
+            <div class="row">
+                <div class="row__time">
+                    <div class="article__time">
+                        <em>
+                            <time datetime="2014-01-17">17 Января 2014</time>
+                        </em>
+                    </div>
+                </div>
+                <div class="row__time">
+                    <div class="article__category">
+                        <em>
+                            <a href="">Программирование</a>
+                        </em>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <p>Программист пишет код в рамках поставленной задачи. В его задачи не входит разрабатывать логику проекта в целом, его главная задача сделать так, чтобы это работало, а по возможности, чтобы хорошо работало.
+                Разработчик может как писать код, так и разрабатывать весь проект в целом: начиная от самостоятельного программирования/проектирования исходных частей проекта, заканчивая координацией проекта и управлением программистами, дизайнерами, PR-менеджерами и тд.
+                <strong>Заголовок</strong>
+            </p>
+        </header>
+        <footer>
+            <a class="button button--main button--sm button--block" href="">Читать далее</a>
+            <div class="alert">
+                <span>Теги:</span>
+                <a class="button button--sm" href="">PHP</a>
+                <a class="button button--sm" href="">Javascript</a>
+            </div>
+        </footer>
+    </article>
+    <article class="article" role="article">
+        <header>
+            <h2><a href="#">Название статьи</a></h2>
+            <div class="row">
+                <div class="row__time">
+                    <div class="article__time">
+                        <em>
+                            <time datetime="2014-01-17">17 Января 2014</time>
+                        </em>
+                    </div>
+                </div>
+                <div class="row__time">
+                    <div class="article__category">
+                        <em>
+                            <a href="">Программирование</a>
+                        </em>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <p>Программист пишет код в рамках поставленной задачи. В его задачи не входит разрабатывать логику проекта в целом, его главная задача сделать так, чтобы это работало, а по возможности, чтобы хорошо работало.
+                Разработчик может как писать код, так и разрабатывать весь проект в целом: начиная от самостоятельного программирования/проектирования исходных частей проекта, заканчивая координацией проекта и управлением программистами, дизайнерами, PR-менеджерами и тд.
+                <strong>Заголовок</strong>
+            </p>
+        </header>
+        <footer>
+            <a class="button button--main button--sm button--block" href="">Читать далее</a>
+            <div class="alert">
+                <span>Теги:</span>
+                <a class="button button--sm" href="">PHP</a>
+                <a class="button button--sm" href="">Javascript</a>
+            </div>
+        </footer>
+    </article>
+    <nav class="alert" role="navigation">
+        <ul class="pagination">
+            <li class="pagination__item"><a href="#" class="pagination__link pagination__link_first"> << first</a></li>
+            <li class="pagination__item"><a href="#" class="pagination__link pagination__link_prev"> < prev</a></li>
+            <li class="pagination__item"><a href="#" class="pagination__link">1</a></li>
+            <li class="pagination__item"><a href="#" class="pagination__link">2</a></li>
+            <li class="pagination__item"><a href="#" class="pagination__link">3</a></li>
+            <li class="pagination__item"><a href="#" class="pagination__link pagination__link_current">4</a></li>
+            <li class="pagination__item"><a href="#" class="pagination__link">5</a></li>
+            <li class="pagination__item"><a href="#" class="pagination__link pagination__link_next">next ></a></li>
+            <li class="pagination__item"><a href="#" class="pagination__link pagination__link_last">last >></a></li>
+        </ul>
+    </nav>
+</section>
--- frontend/views/layouts/front.php
+++ frontend/views/layouts/front.php
@@ -22,7 +22,47 @@ AppAsset::register($this);
     </head>
     <body>
     <?php $this->beginBody() ?>
-        <?= $content ?>
+    <header class="header" role="banner">
+        <h2 class="container header__logo">
+            { Sporthock blog }
+        </h2>
+        <nav class="main-nav" role="navigation">
+            <ul class="main-nav__list">
+                <li class="main-nav__item">
+                    <a class="main-nav__link" href="#">Блог</a>
+                </li>
+                <li class="main-nav__item">
+                    <a class="main-nav__link" href="#">Об авторе</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main class="container" role="main">
+        <div class="row">
+            <?= $content ?>
+            <aside class="row__right">
+                <div class="alert">
+                    <div class="alert__header">Последние записи</div>
+                    <p>
+                        <a class="alert__link" href="">Название статьи</a>
+                    </p>
+                    <p>
+                        <a class="alert__link" href="">Название статьи</a>
+                    </p>
+                </div>
+
+                <div class="alert">
+                    <div class="alert__header">Категории</div>
+                    <p>
+                        <a class="alert__link" href="">Категория 1</a>
+                    </p>
+                    <p>
+                        <a class="alert__link" href="">Категория 2</a>
+                    </p>
+                </div>
+            </aside>
+        </div>
+    </main>
     <?php $this->endBody() ?>
     </body>
 </html>
