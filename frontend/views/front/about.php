e5ebd2f3147d4960f22bac07a6326f5b864406d4
Alexsey <alexsey_89@bk.ru>
2017-01-01 17:17:39 +0300

    #33 Fill page about, add cache requests in prod, deny permission to create files

--- frontend/views/front/about.php
+++ frontend/views/front/about.php
@@ -5,5 +5,9 @@ $this->registerMetaTag(['name' => 'description','content' => Yii::$app->name. ' 
 ?>
 
 <section class="row__left">
+    <h2><?php echo Yii::t('app', 'About');?></h2>
+    <p>Меня зовут Алексей, в блоге буду делится своим мнением и опытом про технологии, веб разработку, операционные системы</p>
+    <p>Так же блог - это место для хранения информации для себя</p>
+    <p>Если у вас есть вопрос, предложение, мнение то пишите на почту <a href="mailto:alex@sporthock.ru">alex@sporthock.ru</a></p>
 </section>
 
--- frontend/views/front/index.php
+++ frontend/views/front/index.php
@@ -3,7 +3,7 @@
 /* @var $dataProvider \yii\data\ActiveDataProvider */
 /* @var $page string*/
 $p = !is_null($page) ? ' - Страница '.$page : '';
-$this->title = Yii::$app->name .' - Разработка сервисов, программирование, самосовершенствование'. $p;
+$this->title = 'Разработка сервисов, программирование, самосовершенствование'. $p;
 $this->registerMetaTag(['name' => 'description','content' => Yii::$app->name. ' – Профессиональное самосовершенствование,технические статьи и заметки.'. $p]);
 ?>
 <section class="row__left">
