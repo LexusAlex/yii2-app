f896a3c4dee9d22124a56089486d5cad4c4bcf49
Alexsey <alexsey_89@bk.ru>
2017-01-11 13:36:47 +0300

    #35 Comments to prod, Add Yandex Metrika, fix bags

--- frontend/views/layouts/front.php
+++ frontend/views/layouts/front.php
@@ -21,6 +21,9 @@ AppAsset::register($this);
     </head>
     <body>
     <?php $this->beginBody() ?>
+    <?php if(YII_ENV === 'prod'){ ?>
+        <!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter23409694 = new Ya.Metrika({ id:23409694, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/23409694" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+    <?php } ?>
     <header class="header" role="banner">
         <h2 class="container header__logo">
             <?php echo Html::a('{ '.Yii::$app->name.' }',['front/index'],['style'=>'text-decoration:none;color:#333333']);?>
